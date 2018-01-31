74f92df93cbbdb553b2da316d399fd25f71e6bdd
Ranjan Rahi <ranjan.rahy@gmail.com>
2018-02-01 00:21:32 +0530

    Ranjan

--- models/trainsModel.js
+++ models/trainsModel.js
@@ -2,12 +2,13 @@ var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
 var trainsSchema = new Schema({
-	'train_code' : String,
+	'train_number' : String,
 	'train_source' : String,
 	'train_destination' : String,
 	'train_name' : String,
-	'depat_atsource' : String,
-	'arrival_atdest' : String
+	'depart_atsource' : String,
+	'arrival_atdest' : String,
+	'days': Array
 });
 
 module.exports = mongoose.model('trains', trainsSchema);
